1aa2056e0205061a264ad4e184f46a671018d9b5
Noxionx <nox35400@gmail.com>
2014-11-29 12:20:02 +0100

    Added infos alert

--- app/js/controllers/headerCtrl.js
+++ app/js/controllers/headerCtrl.js
@@ -1,4 +1,17 @@
 define(["angular", "angular-material", "angular-route", "./controllers"],function(){
-    angular.module("controllers").controller("headerCtrl",["$scope", function($scope){
+    angular.module("controllers").controller("headerCtrl",["$scope", "$mdDialog", function($scope, $mdDialog){
+            
+         $scope.showInfos = function(ev) {
+            $mdDialog.show({
+              controller: infosCtrl,
+              templateUrl: 'partials/infos.html',
+              targetEvent: ev,
+            });
+        };
     }]);
+    function infosCtrl($scope, $mdDialog) {
+            $scope.hide = function() {
+              $mdDialog.hide();
+            };
+        }
 });
